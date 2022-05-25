309c5b6716c098e297aa64019dcc5595a1abbb8a
onirutlA <alturino001@gmail.com>
2022-05-25 12:29:27 +0700

    add: lemonade-app

---
+++ lemonade-app/app/src/main/java/com/onirutla/lemonade_app/MainActivity.kt
@@ -0,0 +1,150 @@
+package com.onirutla.lemonade_app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.onirutla.lemonade_app.ui.theme.LemonadeAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LemonadeAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    LemonadeApp()
+                }
+            }
+        }
+    }
+}
+
+
+@Preview
+@Composable
+fun LemonadeApp() {
+    var currentStep by remember { mutableStateOf(1) }
+    var squeezeCount by remember { mutableStateOf(0) }
+
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colors.background
+    ) {
+        when (currentStep) {
+            1 -> {
+                LemonTextAndImage(
+                    textLabelResourceId = R.string.first,
+                    drawableResourceId = R.drawable.lemon_tree,
+                    onImageClick = {
+                        currentStep = 2
+                        squeezeCount = (2..4).random()
+                    }
+                )
+            }
+            2 -> {
+                LemonTextAndImage(
+                    textLabelResourceId = R.string.second,
+                    drawableResourceId = R.drawable.lemon_squeeze,
+                    onImageClick = {
+                        squeezeCount--
+                        if (squeezeCount == 0) {
+                            currentStep = 3
+                        }
+                    }
+                )
+            }
+            3 -> {
+                LemonTextAndImage(
+                    textLabelResourceId = R.string.third,
+                    drawableResourceId = R.drawable.lemon_drink,
+                    onImageClick = {
+                        currentStep = 4
+                    }
+                )
+            }
+            4 -> {
+                LemonTextAndImage(
+                    textLabelResourceId = R.string.fourth,
+                    drawableResourceId = R.drawable.lemon_restart,
+                    onImageClick = {
+                        currentStep = 1
+                    }
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun LemonTextAndImage(
+    modifier: Modifier = Modifier,
+    textLabelResourceId: Int,
+    drawableResourceId: Int,
+
+    onImageClick: () -> Unit,
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier.fillMaxSize()
+    ) {
+        Text(
+            text = stringResource(textLabelResourceId),
+            fontSize = 18.sp
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Image(
+            modifier = Modifier
+                .wrapContentSize()
+                .clickable(
+                    onClick = onImageClick
+                )
+                .border(
+                    BorderStroke(2.dp, Color(105, 205, 216)),
+                    shape = RoundedCornerShape(4.dp)
+                )
+                .padding(16.dp),
+            painter = painterResource(id = drawableResourceId),
+            contentDescription = null
+        )
+    }
+}
+
+@Preview
+@Composable
+fun LemonPreview() {
+    LemonadeAppTheme {
+        LemonadeApp()
+    }
+}
